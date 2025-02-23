2b994d11743e5225fbadecd9ace198a3b1d06a5c
dinadendan <dina.dendan222@gmail.com>
2025-02-23 20:29:49 +0200

    - add flutter_tawk package - add CustomServiceScreen

--- lib/core/theming/app_colors.dart
+++ lib/core/theming/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors{
 
   static const Color mainRed=Color(0xFFEF2A39);
+  // static const Color mainRed=Color(0xFFa80322);
   static const Color white=Color(0xFFFFFFFF);
   static const Color grey=Color(0xFF6A6A6A);
   static const Color lightGrey=Color(0xFFF3F4F6);
--- lib/features/custom_service/ui/custom_service_screen.dart
+++ lib/features/custom_service/ui/custom_service_screen.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_tawkto/flutter_tawk.dart';
 
 class CustomServiceScreen extends StatelessWidget{
   const CustomServiceScreen({super.key});
+  static const String _chatLink =
+      'https://tawk.to/chat/67b338e01a702b1909007eb6/1ika1cb6i';
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.only(bottom: 10.h),
+          child: Tawk(
+            directChatLink: _chatLink,
+            visitor: TawkVisitor(
+              name: 'UserName',
+              email: 'userEmail@gmail.com',
+            ),
+            onLoad: () => debugPrint('chat loaded'),
+            onLinkTap: (String url) => debugPrint('Tapped URL: $url'),
+            placeholder: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        ),
+      ),
+    );
+
   }
 }
