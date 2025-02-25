20bbf500fdf7721b5b8e5c309f123814c51fb53d
dinadendan <dina.dendan222@gmail.com>
2025-02-25 17:48:04 +0200

    - add image of user profile - add Profile Settings Screen - add AppTextFormField

---
+++ lib/core/shared_widgets/app_text_field.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_go/core/theming/app_colors.dart';
+import 'package:food_go/core/theming/app_text_styles.dart';
+
+class AppTextFormField extends StatelessWidget {
+  final EdgeInsetsGeometry? contentPadding;
+  final InputBorder? focusedBorder;
+  final InputBorder? enabledBorder;
+  final InputBorder? errorBorder;
+  final InputBorder? focusedErrorBorder;
+  final TextStyle? inputTextStyle;
+  final TextStyle? hintStyle;
+  final String hintText;
+  final String label;
+  final bool? isObscureText;
+  final Widget? suffixIcon;
+  final Color? backgroundColor;
+  final TextEditingController? controller;
+  final Function(String?) validator;
+  final FocusNode? focusNode;
+  const AppTextFormField(
+      {super.key,
+        this.contentPadding,
+        this.focusedBorder,
+        this.enabledBorder,
+        this.inputTextStyle,
+        this.hintStyle,
+        required this.hintText,
+        this.isObscureText,
+        this.suffixIcon,
+        this.backgroundColor,
+        this.controller,
+        required this.validator,
+        this.focusNode,
+        this.errorBorder,
+        this.focusedErrorBorder,
+        required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      focusNode: focusNode,
+      decoration: InputDecoration(
+        labelText: label,
+        isDense: true,
+        contentPadding: contentPadding ??
+            EdgeInsets.symmetric(horizontal: 20.w, vertical: 16.h),
+        errorBorder: errorBorder ??
+            OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: AppColors.mainRed,
+                width: 1.5,
+              ),
+              borderRadius: BorderRadius.circular(25.r),
+            ),
+        focusedErrorBorder: focusedErrorBorder ??
+            OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: AppColors.mainRed,
+                width: 1.5,
+              ),
+              borderRadius: BorderRadius.circular(25.r),
+            ),
+        focusedBorder: focusedBorder ??
+            OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: AppColors.mainRed,
+                width: 1.5,
+              ),
+              borderRadius: BorderRadius.circular(25.r),
+            ),
+        enabledBorder: enabledBorder ??
+            OutlineInputBorder(
+              borderSide: const BorderSide(
+                color: Colors.grey,
+                width: 1.5,
+              ),
+              borderRadius: BorderRadius.circular(25.r),
+            ),
+        hintStyle: hintStyle ?? AppTextStyles.font18Grey.copyWith(color: Colors.grey[400]),
+        hintText: hintText,
+        suffixIcon: suffixIcon,
+        filled: true,
+        fillColor: backgroundColor ?? Colors.white,
+      ),
+      obscureText: isObscureText ?? false,
+      style: inputTextStyle ?? AppTextStyles.font18Grey,
+      validator: (value) {
+        return validator(value);
+      },
+    );
+  }
+}
--- lib/features/profile_setting/ui/profile_settings.dart
+++ lib/features/profile_setting/ui/profile_settings.dart
@@ -1,10 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:food_go/core/helper/spacing.dart';
+import 'package:food_go/core/shared_widgets/app_text_field.dart';
+import 'package:food_go/core/theming/app_text_styles.dart';
 
 class ProfileSettingsScreen extends StatelessWidget{
   const ProfileSettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final formKey = GlobalKey<FormState>();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children:[
+            Padding(
+              padding: EdgeInsets.only(top: 40.h ),
+              child: Center(
+                child: CircleAvatar(
+                  radius: 80.r,
+                  backgroundColor:Colors.grey[200],
+                  backgroundImage: const AssetImage('assets/images/user.png'),
+              ),
+            ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.w,vertical: 20.h),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    AppTextFormField(
+                      label: 'Name',
+                        hintText: 'Enter your name',
+                        validator: (value){}
+                    ),
+                    verticalSpace(20),
+                    AppTextFormField(
+                        label: 'Email',
+                        hintText: 'Enter your email',
+                        validator: (value){}
+                    ),
+                    verticalSpace(20),
+                    AppTextFormField(
+                        label: 'Address',
+                        hintText: 'Enter your address',
+                        validator: (value){}
+                    ),
+                    verticalSpace(20),
+                    AppTextFormField(
+                        label: 'Password',
+                        hintText: 'Enter your password',
+                        validator: (value){},
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.w,vertical: 20.h),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                        'Log Out',
+                        style: AppTextStyles.font20Black.copyWith(
+                          fontWeight: FontWeight.w600,
+                        )),
+                    const Icon(Icons.logout,
+                    size: 26,),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
